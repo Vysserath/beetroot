148e655aca47b10845c38c73d49bcdd7a922c040
Vysserath <vysserath@gmail.com>
2022-04-23 10:18:40 +0300

    js

--- src/js/lesson.js
+++ src/js/lesson.js
@@ -1,80 +1,35 @@
-/* Створи масив «Список покупок». Кожен елемент масиву є об'єктом, який містить назву продукту, кількість і куплений він чи ні, ціну за одиницю товару, сума. Написати кілька функцій для роботи з таким масивом:
-    1 Виводити весь список на екран таким чином, щоб спочатку йшли продукти, що ще не придбані, а потім - ті, що вже придбали.
-    2 Покупка продукту. Функція приймає назву продукту і відзначає його як придбаний.
-    3 Створення списку (не) придбаних продуктів. */ 
-
-let buyList = [
-    {
-        name: 'milk',
-        amount: 1,
-        isBought: true,
-        price: 30,
-        sum: this.price * this.amount
-    },
-    {
-        name: 'bread',
-        amount: 3,
-        isBought: false,
-        price: 20,
-        sum: this.price * this.amount
-    },
-    {
-        name: 'pizza',
-        amount: 1,
-        isBought: false,
-        price: 150,
-        sum: this.price * this.amount
-    },
-    {
-        name: 'water',
-        amount: 5,
-        isBought: true,
-        price: 10,
-        sum: this.price * this.amount
+class Circle {
+    constructor(rad){
+        this.rad = rad;
     }
-];
 
-//Виводити весь список на екран таким чином, щоб спочатку йшли продукти, що ще не придбані, а потім - ті, що вже придбали.
+    get rad() {
+        return this._rad;
+    }
 
-function displayUnbought(arr) {
-    arr.sort(function (a, b) {
-        if (a.isBought > b.isBought) {
-            return 1;
-        };
-        if (a.isBought < b.isBought) {
-            return -1;
-        };
-        return 0;
-    });
-    console.log(arr);
-};
+    get diam() {
+        return this.rad * 2;
+    }
 
-displayUnbought(buyList);
-
-// Покупка продукту. Функція приймає назву продукту і відзначає його як придбаний.
-
-function buy(name, arr) {
-    for(let i = 0; i < arr.length; i++) {
-        if (arr[i].name == name) {
-            arr[i].isBought = true;
+    set rad(value) {
+        if (value > 0) {
+            this._rad = value;
         }
     }
-};
 
-buy('pizza', buyList);
-console.log(buyList);
+    squareCircle() {
+        console.log(Math.PI * this._rad**2);
+    }
 
-// Створення списку (не) придбаних продуктів.
-
-let newList = [];
-
-function  createUnboughtList(arr) {
-    for(i in arr) {
-        if (arr[i].isBought == false) {
-            newList.push(arr[i]);
-        }
+    lengthCircle() {
+        console.log(Math.PI**2 * this.rad);
     }
 }
 
-createUnboughtList(buyList);
-console.log(newList);
+let crug = new Circle(5);
+
+console.log(crug.diam);
+
+crug.squareCircle();
+crug.lengthCircle();
+
