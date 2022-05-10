7aa3d469ca938821bccc86411a9008b2292072e7
Vysserath <vysserath@gmail.com>
2022-05-10 20:05:25 +0300

    slider

--- src/js/lesson.js
+++ src/js/lesson.js
@@ -1,39 +1,16 @@
-const main = document.querySelector('.main');
-const images = document.querySelector('#images-container');
-
-
-fetch('https://dummyjson.com/products/2').then((response) => {
-    return response.json();
-}).then((data) => {
-    let template = `
-    <div class="container">
-        <div class="img-holder">
-            <img src="${data.thumbnail}" alt="" class="img img_main">
-        </div>
-        <div class="text-block">
-            <h1 class="text-block__title">${data.title}</h1>
-            <ul class="text-block__list">
-                <li class="text-block__list-item">Brand: <a href="#">${data.brand}</a></li>
-                <li class="text-block__list-item">Price: ${data.price}$</li>
-                <li class="text-block__list-item">Rating: ${data.rating}</li>
-                <li class="text-block__list-item">Categoty: <a href="#">${data.category}</a></li>
-                <li class="text-block__list-item">Description: ${data.description}</li>
-            </ul>
-            <div class="link-holder">
-                <a href="#" class="text-block__link">Buy</a>
-            </div>
-        </div>
-    </div>`
-
-    main.insertAdjacentHTML('afterbegin', template);
-
-    data.images.map((i) => {
-        let templateImg = `
-        <div class="img-holder img-holder_little">
-            <img src="${i}" alt="" class="img">
-        </div>`
-
-        images.insertAdjacentHTML('beforeend', templateImg);
-    })
-
-})
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper__pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+    },
+  
+  });
